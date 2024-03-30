c8fd3c7a92894c9a6388868bdd666dbdf640aa92
NeverDh <63487149+NeverDh@users.noreply.github.com>
2024-03-30 20:26:11 -0300

    Rota login retorna o token basic

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -1,23 +1,23 @@
 const model = require("../models/user.model");
 
 const authorization = async (req, res) => {
-	if (!req.get("Authorization")){
-		return res.status(401).json({message: "Não Autorizado!"});
-	}
-	const credentials = Buffer.from(req.get("Authorization").split(" ")[1], "base64").toString().split(":");
+    if (!req.get("Authorization")) {
+        return res.status(401).json({ message: "Não Autorizado!" });
+    }
+    const encodedCredentials = req.get("Authorization").split(" ")[1];
+    const decodedCredentials = Buffer.from(encodedCredentials, "base64").toString();
+    const [username, password] = decodedCredentials.split(":");
 
-	const username = credentials[0];
-	const password = credentials[1];
-	const authorization = await model.loginSQL(username, password);
-	if (authorization === false){
-		return res.status(400).json({message: "Não Autorizado!"});
-	}
-	
-	const allResults = authorization;
+    const authorization = await model.loginSQL(username, password);
+    if (authorization === false) {
+        return res.status(400).json({ message: "Não Autorizado!" });
+    }
 
-	return allResults.length > 0 ? res.status(200).json(allResults) : res.status(400).json({message: "Sem dados!"});
+    const allResults = encodedCredentials;
+
+    return authorization.length > 0 ? res.status(200).json(allResults) : res.status(400).json({ message: "Sem dados!" });
 };
 
 module.exports = {
-	authorization
+    authorization
 };
