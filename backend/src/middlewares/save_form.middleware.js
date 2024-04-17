e17902e2dde26f27888c74f5f89ebbbc5cf5f67b
NeverDh <63487149+NeverDh@users.noreply.github.com>
2024-04-17 19:27:02 -0300

    Adicionado a pedido do Thauan

--- backend/src/middlewares/save_form.middleware.js
+++ backend/src/middlewares/save_form.middleware.js
@@ -20,13 +20,6 @@ const validateSaveForm = async (req, res, next) => {
 		return res.status(400).json({message: "O campo origem não pode ser vazio!"});
 	}
 
-	if(body.temPlano === undefined) {
-		return res.status(400).json({message: "O campo temPlano é obrigatório!"});
-	}
-	if(body.temPlano === "") {
-		return res.status(400).json({message: "O campo temPlano não pode ser vazio!"});
-	}
-
 	if(body.sexo === undefined) {
 		return res.status(400).json({message: "O campo sexo é obrigatório!"});
 	}
